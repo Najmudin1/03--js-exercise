2cf6cc2492c690f6a432dde008090b8e00c2484c
Najmudin Saqib <P15225267@LEC-ADMIN.dmu.ac.uk>
2017-10-19 10:40:57 +0100

    finished exercise

--- js/greeting.js
+++ js/greeting.js
@@ -2,9 +2,13 @@
   let myName = document.getElementById("my-name"); 
   let userName = document.getElementById("user-name"); 
   let getName = document.getElementById("get-name");
+  let myHeader = document.getElementById("my-header");
+   let lastname = document.getElementById("last-name");
   
   function PerformGreeting() {
     myName.innerHTML = userName.value;
+	lastname.innerHTML = last-name.value;
+	
     event.preventDefault();
     return false;
   }
